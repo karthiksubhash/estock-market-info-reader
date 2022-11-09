f4f0ee7942816d5a5e5e8c06f55cb505e2a0e216
Karthik <karthiks7678@gmail.com>
2022-11-09 23:06:35 +0530

    Update CompanyStockInfoAPI.java

--- src/main/java/com/EStockMarketApp/EStockMarketInfoReader/api/CompanyStockInfoAPI.java
+++ src/main/java/com/EStockMarketApp/EStockMarketInfoReader/api/CompanyStockInfoAPI.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.EStockMarketApp.EStockMarketInfoReader.dto.EstockInfoModelDTO;
 import com.EStockMarketApp.EStockMarketInfoReader.service.CompanyStockInfoService;
 
-
+@CrossOrigin
 @RestController
 public class CompanyStockInfoAPI {
 
